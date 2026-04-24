4a4557d860f95bc7ff413e829479a619df5b8d1e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-24 10:34:19 -0300

    Adiciona entrada de notas e calculo da media

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    float n1 = 0, n2 = 0, media = 0;
     int opcao = 0;
 
     while(opcao != 5) {
@@ -16,9 +17,25 @@ int main() {
         scanf("%d", &opcao);
 
         switch(opcao) {
+
+            case 1:
+                printf("Nota 1: ");
+                scanf("%f", &n1);
+                printf("Nota 2: ");
+                scanf("%f", &n2);
+                break;
+
+            case 2:
+                media = (n1 + n2) / 2;
+                printf("Media: %.2f\n", media);
+                break;
+
             case 5:
                 printf("Saindo...\n");
                 break;
+
+            default:
+                printf("Opcao invalida!\n");
         }
     }
 
