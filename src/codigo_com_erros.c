0261a6a9ba0577aaa81a8536a3962f8a86d67e2e
João Marcelo de Melo e Silva <jhony.melo.silva@gmail.com>
2026-04-24 08:48:56 -0300

    C:\Users\labsfiap\Desktop\CP2_DSA\CP2-1CCPG-DSA

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,15 +1,16 @@
 #include "stdio.h"
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media;
 
-    printf("Digite a primeira nota: ");
+    
+    printf("Digite a primeira nota: \n");
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
     scanf("%f", nota2);
 
-    media = nota1 + nota2 / 2;
+    media = (nota1 + nota2) / 2;
 
     if (media = 6) {
         printf("Aluno aprovado\n");
