5984add2558cffbf3d297f08d45f65d865af3ddd
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-24 10:33:30 -0300

    Criacao do menu do sistema CCPG

--- src/main.c
+++ src/main.c
@@ -1,8 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    
+    int opcao = 0;
 
-    
+    while(opcao != 5) {
+        printf("\n========================\n");
+        printf("  SISTEMA EQUIPE CCPG\n");
+        printf("========================\n");
+        printf("1 - Inserir notas\n");
+        printf("2 - Calcular media\n");
+        printf("3 - Verificar situacao\n");
+        printf("4 - Exibir resultado\n");
+        printf("5 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 5:
+                printf("Saindo...\n");
+                break;
+        }
+    }
+
     return 0;
 }
