90142f0cf7fa49b53e67949e381b46818230b92b
João Marcelo de Melo e Silva <jhony.melo.silva@gmail.com>
2026-04-24 22:59:14 -0300

    Correção e explicação dos Erros

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,16 +1,16 @@
-#include "stdio.h"
+#include <stdio.h> //Alteração das aspas ("") para <> no stdio.h
 
 int main() {
-    float nota1, nota2, media;
+    float nota1, nota2, media; //Adição de um ponto e virgula (;) ao final da linha
 
     
     printf("Digite a primeira nota: \n");
     scanf("%f", &nota1);
 
-    printf("Digite a segunda nota: ");
+    printf("Digite a segunda nota: \n"); //Adição de um '\n' para melhor visualização no terminal
     scanf("%f", nota2);
 
-    media = (nota1 + nota2) / 2;
+    media = (nota1 + nota2) / 2; //Adição de paranteses entre 'nota1 + nota2'
 
     if (media = 6) {
         printf("Aluno aprovado\n");
